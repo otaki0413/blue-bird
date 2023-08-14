71737b3401fc55a7534b5571f86de151733750fc
otaki0413 <takida0413@gmail.com>
2023-08-14 15:45:08 +0900

    ✨ ミドルウェアを作成し、期限切れのSessionを更新できるようにした

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient({ req, res });
+  // セッションが期限切れであれば更新
+  await supabase.auth.getSession();
+
+  return res;
+}
