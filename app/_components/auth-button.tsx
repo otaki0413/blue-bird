1383b4ca94a1bd2c81b95dfc22a0426a736bd4ff
otaki0413 <takida0413@gmail.com>
2023-08-14 15:46:30 +0900

    ✨ サインアウト後にサーバーコンポーネントを再実行する処理を追加

--- app/_components/auth-button.tsx
+++ app/_components/auth-button.tsx
@@ -1,13 +1,17 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 export const AuthButton = () => {
   const supabase = createClientComponentClient();
+  const router = useRouter();
 
   // サインアウト処理
   const handleSignOut = async () => {
     await supabase.auth.signOut();
+    // ページをリフレッシュ
+    router.refresh();
   };
 
   // サインイン処理
